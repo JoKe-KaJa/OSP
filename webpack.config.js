be4fbb167c649354e7d27fc0adb03c49578a1376
Johnny Chan <johnnychan2011@gmail.com>
2022-07-04 23:50:55 -0700

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+  },
+
+  devServer: {
+    historyApiFallback: true,
+    //refresh code;
+
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.s?css$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './index.html',
+    }),
+  ],
+};
